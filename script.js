0e1a9c1c594f8d7967993f0a992cf81a080a714a
JohnPaul-Londono <johnplondono@gmail.com>
2021-10-19 00:05:07 -0500

    Likes button Update

--- script.js
+++ script.js
@@ -1,40 +1,47 @@
-// var count = 9;
-// var countElement = document.querySelector(".count")
+// console.log("page loading...");
+
+// index     0  1   2
+var post = [9, 12, 10];
+var ps = [
+    document.querySelector("#count"),
+    document.querySelector("#count1"),
+    document.querySelector("#count2")
+];
+
+function add1(id) {
+    post[id]++;
+    ps[id].innerHTML = post[id] + "like(s)";
+}
 
 
 
-// console.log(countElement);
+
 
 // function add1(){
+//     var count = 9;
+//     var countElement = document.querySelector(".count")
+//     console.log(countElement);
 //     count++;
 //     countElement.innerText = count + " like(s)";
 //     console.log(count);
-
-function add1(){
-    var count = 9;
-    var countElement = document.querySelector(".count")
-    console.log(countElement);
-    count++;
-    countElement.innerText = count + " like(s)";
-    console.log(count);
     
-}
+// }
 
-function add2(){
-    var count = 12;
-var countElement = document.querySelector(".count1")
-console.log(countElement);
-    count++;
-    countElement.innerText = count + " like(s)";
-    console.log(count);
-}
-function add3(){
-    var count = 10;
-var countElement = document.querySelector(".count2")
-console.log(countElement);
-    count++;
-    countElement.innerText = count + " like(s)";
-    console.log(count);
-}
+// function add2(){
+//     var count = 12;
+// var countElement = document.querySelector(".count1")
+// console.log(countElement);
+//     count++;
+//     countElement.innerText = count + " like(s)";
+//     console.log(count);
+// }
+// function add3(){
+//     var count = 10;
+// var countElement = document.querySelector(".count2")
+// console.log(countElement);
+//     count++;
+//     countElement.innerText = count + " like(s)";
+//     console.log(count);
+// }
 
 
