52a2b24e52b43b638dcf804d7c36623fcb49ecff
JohnPaul-Londono <johnplondono@gmail.com>
2021-10-18 23:24:17 -0500

    Likes Button

---
+++ script.js
@@ -0,0 +1,40 @@
+// var count = 9;
+// var countElement = document.querySelector(".count")
+
+
+
+// console.log(countElement);
+
+// function add1(){
+//     count++;
+//     countElement.innerText = count + " like(s)";
+//     console.log(count);
+
+function add1(){
+    var count = 9;
+    var countElement = document.querySelector(".count")
+    console.log(countElement);
+    count++;
+    countElement.innerText = count + " like(s)";
+    console.log(count);
+    
+}
+
+function add2(){
+    var count = 12;
+var countElement = document.querySelector(".count1")
+console.log(countElement);
+    count++;
+    countElement.innerText = count + " like(s)";
+    console.log(count);
+}
+function add3(){
+    var count = 10;
+var countElement = document.querySelector(".count2")
+console.log(countElement);
+    count++;
+    countElement.innerText = count + " like(s)";
+    console.log(count);
+}
+
+
